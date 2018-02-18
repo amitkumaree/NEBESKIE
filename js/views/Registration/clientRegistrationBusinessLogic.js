5c0d350c3edc003db5480cd605a62406f4635332
amitkumaree <mail.amitk1@gmail.com>
2018-02-18 16:58:38 +0530

    Added a new registration page

---
+++ js/views/Registration/clientRegistrationBusinessLogic.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    debugger;
+    hideItemsOnInitialLoad();
+    
+})()
+
+/* hide items on page load*/
+function hideItemsOnInitialLoad() {
+    $("#success_message").toggle();
+}
+
+/* submit event click handler*/
+function submitBtnClick() {
+    debugger;
+    alert("submit is clicked");
+}
